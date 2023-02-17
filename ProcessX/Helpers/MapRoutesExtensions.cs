8a1c25d466253ab822410433f3794f1a18d35f02
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-02-18 01:24:43 +0200

    created mapRoutes Extentions

---
+++ ProcessX/Helpers/MapRoutesExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ProcessX.Helpers
+{
+    internal static class MapRoutesExtensions
+    {
+        public static IEndpointRouteBuilder MapActions(this IEndpointRouteBuilder routes)
+        {
+            var api = routes.MapGroup("/api");
+
+            var jsonContentType = MediaTypeNames.Application.Json;
+
+
+            api.MapGet("/", () => Results.Ok(DateTimeOffset.UtcNow));
+            return routes;
+        }
+    }
+}
--- ProcessX/Program.cs
+++ ProcessX/Program.cs
@@ -16,28 +16,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapActions();
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/time", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return Results.Ok(DateTime.UtcNow);
 })
-.WithName("GetWeatherForecast");
+.WithName("time");
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
