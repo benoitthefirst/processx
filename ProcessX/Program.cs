021cf3e8c27c2ac19e17ba99cde9086478021920
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-02-18 01:32:37 +0200

    added services and actions to depedency

--- ProcessX/Program.cs
+++ ProcessX/Program.cs
@@ -1,9 +1,91 @@
+Env.Load();
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins(
+                "http://localhost:3000",
+                "https://processx.theprocesse.com/").AllowAnyMethod().AllowAnyHeader();
+        }
+    );
+});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IPasswordService, Argon2PasswordService>();
+builder.Services.AddSingleton<IAction<RegisterRequest>, RegisterAction>();
+builder.Services.AddSingleton<IMongoDatabase>((sp) =>
+{
+    //var clientSettings = new MongoClientSettings() { };
+    var clientSettings = MongoClientSettings.FromConnectionString(Configs.DbHost);
+
+    // checking that credentials were provided
+    if (!string.IsNullOrWhiteSpace(Configs.DbUsername) && !string.IsNullOrWhiteSpace(Configs.DbUserPassword))
+    {
+        var mongoCred = MongoCredential.CreateCredential(Configs.DbAuthDb, Configs.DbUsername, Configs.DbUserPassword);
+        clientSettings.Credential = mongoCred;
+    }
+    var client = new MongoClient(clientSettings);
+    return client.GetDatabase(Configs.DbName);
+});
+
+builder.Services.AddSingleton<IAuthService>(sp =>
+    new JwtAuthService(Configs.JwtIssuer,
+    Configs.JwtAudience,
+    Configs.JwtValidateIssuer,
+    Configs.JwtValidateAudience));
+
+builder.Services.AddTransient<IAuthContainerModel>(sp =>
+    new JwtContainerModel(Configs.JwtAccessTokenSecretKey,
+    Configs.JwtSecurityAlgorithm,
+    Configs.JwtAccessTokenExpiresIn));
+
+builder.Services.AddTransient<IAuthRefreshTokenContainerModel>(sp =>
+    new JwtContainerModel(Configs.JwtRefreshTokenSecretKey,
+    Configs.JwtSecurityAlgorithm,
+    Configs.JwtRefreshTokenExpiresIn));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddScheme<JwtBearerOptions, JwtAuthenticationHandlerAuth>(JwtBearerDefaults.AuthenticationScheme, default);
+
+builder.Services.AddAuthorization(config =>
+{
+    config.AddPolicy(ConfigAuthPolicies.RefreshTokenPolicy, policyBuilder =>
+    {
+        policyBuilder.RequireClaim(ClaimTypes.Role);
+        policyBuilder.RequireRole(ConfigAuthPolicies.Roles.RefreshToken);
+    });
+
+    config.AddPolicy(ConfigAuthPolicies.AccessTokenPolicy, policyBuilder =>
+    {
+        policyBuilder.RequireClaim(ClaimTypes.Role);
+        policyBuilder.RequireRole(ConfigAuthPolicies.Roles.AccessToken);
+    });
+
+    config.AddPolicy(ConfigAuthPolicies.AdminViewerTokenPolicy, policyBuilder =>
+    {
+        policyBuilder.RequireClaim(ClaimTypes.Role);
+        policyBuilder.RequireRole(
+        ConfigAuthPolicies.Roles.AdminToken,
+        ConfigAuthPolicies.Roles.AdminManagerToken,
+        ConfigAuthPolicies.Roles.AdminViewerToken);
+    });
+});
+builder.Services.AddSingleton(typeof(CollectionProvider<>));
 
 var app = builder.Build();
 
