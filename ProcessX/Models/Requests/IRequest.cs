11a28c3f465c88ef93e9214710fd7d80a6a0f847
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-02-17 23:56:43 +0200

    created business register request model

---
+++ ProcessX/Models/Requests/IRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ProcessX.Models.Requests
+{
+    internal interface IRequest
+    {
+        internal bool IsValidRequest { get; }
+        internal DateTimeOffset Now { get; set; }
+    }
+}
---
+++ ProcessX/Models/Requests/RegisterRequest.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.Runtime.Internal;
+
+namespace ProcessX.Models.Requests
+{
+    public record RegisterRequest(
+        string FirstName,
+        string LastName,
+        string Password,
+        string Email,
+        string telephoneNumber,
+        string category,
+        TradingAddress tradingAddress,
+        CompanyType companyType = CompanyType.PrivateCompany,
+        bool acceptCard = false
+        ) : IRequest
+    {
+
+        public bool IsValidRequest => IsValidUpdateRequest();
+
+        public DateTimeOffset Now { get; set; } = DateTimeOffset.UtcNow;
+        public bool IsValidUpdateRequest()
+        {
+            if (string.IsNullOrWhiteSpace(FirstName) ||
+                string.IsNullOrWhiteSpace(LastName) ||
+                string.IsNullOrWhiteSpace(Password) ||
+                string.IsNullOrWhiteSpace(telephoneNumber) ||
+                string.IsNullOrWhiteSpace(Email))
+                return false;
+
+            return true;
+
+        }
+    }
+}
