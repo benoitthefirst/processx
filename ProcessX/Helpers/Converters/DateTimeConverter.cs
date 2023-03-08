b900efa45f0578624a62f10f3f5f6e61b4b80e9b
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-03-08 21:42:18 +0200

    created date converter

---
+++ ProcessX/Helpers/Converters/DateTimeConverter.cs
@@ -0,0 +1,43 @@
+﻿namespace ProcessX.Helpers.Converters;
+
+public class DateTimeConverter : JsonConverter<DateTime>
+{
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        try
+        {
+            return DateTime.Parse(reader.GetString());
+        }
+        catch
+        {
+            return default;
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
+public class NullableDateTimeConverter : JsonConverter<DateTime?>
+{
+    public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        try
+        {
+            return DateTime.Parse(reader.GetString());
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+    {
+        if (value is null)
+            writer.WriteNullValue();
+        else
+            writer.WriteStringValue(value.ToString());
+    }
+}
