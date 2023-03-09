cad4799e0d50adf69d0205250fb5fb7303d0d242
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-03-09 14:43:50 +0200

    created order action

---
+++ ProcessX/Actions/CreateOrderAction.cs
@@ -0,0 +1,28 @@
+﻿namespace ProcessX.Actions
+{
+    public class CreateOrderAction : IAction<Order>
+    {
+        private readonly IMongoCollection<Order> _orders;
+        private readonly ILogger<GetInventoriesAction> _logger;
+        public CreateOrderAction(
+            CollectionProvider<Order> ordersCollectionProvider,
+            ILogger<GetInventoriesAction> logger)
+        {
+            _orders = ordersCollectionProvider.GetCollection();
+            _logger = logger;
+        }
+        public async Task<Response> PerformAction(Order request)
+        {
+            /*if (request.IsValidRequest)
+                return new("Invalid request", HttpStatusCode.BadRequest);*/
+            Order order = new()
+            {
+
+            };
+
+            //await _orders.InsertOneAsync(data);
+
+            return new(order, HttpStatusCode.OK);
+        }
+    }
+}
